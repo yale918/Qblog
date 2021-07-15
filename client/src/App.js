f6bdb4499329c4076db2b2bb1928119d62b39e16
yale918 <yale918@gmail.com>
2021-07-16 00:35:53 +0800

    repair iframe issue

--- client/src/App.js
+++ client/src/App.js
@@ -35,30 +35,12 @@ function App() {
             </div>
           </nav>
 
-
-
-
-
-
-
           <section className="main">
           <Route exact path="/">
-            <iframe
-                title="profile"
-                src="https://yale918.github.io/profile"
-                height="1000px"
-                width="1600px"
-              >
-              </iframe>
+
             </Route>
             <Route path="/cv">
-            <iframe
-                title="profile"
-                src="https://yale918.github.io/profile"
-                height="1000px"
-                width="1600px"
-              >
-              </iframe>
+
             </Route>
             <Route path="/layout">切版練習
               <ul id="dropdown1" className="dropdown-content">
@@ -72,27 +54,11 @@ function App() {
             <Route path="/blog">...部落格 施工中...</Route>
             <Route path="/ttest">
               <div style={{margin:"10px"}}>
-                <a href="https://yale918.github.io/asp" target="_blank" rel="noreferrer"> PS4  </a>|
-                <a href="https://yale918.github.io/nfclone" target="_blank" rel="noreferrer"> NETFLIX  </a>
+
                 <a href="#3"></a>
               </div>
 
-              <iframe
-                title="PS4"
-                src="https://yale918.github.io/asp"
-                height="1000px"
-                width="1600px"
-              >
-              </iframe>
-              <br />
-              <br />
-              <iframe
-                title="NETFLIX"
-                src="https://yale918.github.io/nfclone"
-                height="1000px"
-                width="1600px"
-              >
-              </iframe>
+
             </Route>
           </section>
         </div>
